2822bbf6328bdf2cdd3f04c9aec225502e95d8a5
rickyrivaldo98@gmail.com <rickyrivaldo98@gmail.com>
2021-02-18 23:24:10 +0700

    navbar fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import Dropdown from "./components/Dropdown";
 import Hero from "./components/Hero";
 import Footer from "./layout/Footer";
 import Navbar from "./layout/Navbar";
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  useEffect(() => {
+    const hideMenu = () => {
+      if (window.innerWidth > 768 && isOpen) {
+        setIsOpen(false);
+      }
+    };
+    window.addEventListener("resize", hideMenu);
+    return () => {
+      window.removeEventListener("resize", hideMenu);
+    };
+  });
   return (
     <div className="bg-black">
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Dropdown isOpen={isOpen} toggle={toggle} />
       <Hero />
       <Footer />
     </div>
