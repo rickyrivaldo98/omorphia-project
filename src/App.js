e6c2dbe5d7132d7e78d891e20384f6cfe8f334c8
rickyrivaldo98@gmail.com <rickyrivaldo98@gmail.com>
2021-02-25 19:57:10 +0700

    header dan compare

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import Hero from "./components/Hero";
 import Gallery from "./components/Gallery";
 import Footer from "./layout/Footer";
 import Navbar from "./layout/Navbar";
+import Bgatas1 from "./assets/image/bg1.png";
+import Bgatas2 from "./assets/image/bg2.png";
+import Pic1 from "./assets/image/pic1.svg";
+import Pic2 from "./assets/image/pic2.svg";
+import Pic3 from "./assets/image/pic3.svg";
+import Timeslide from "./components/Timeslide";
 
-
-
-function App() {
+export const App = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [image1, setImage1] = useState("");
+  const [image2, setImage2] = useState("");
+  const [image3, setImage3] = useState("");
 
   const toggle = () => {
     setIsOpen(!isOpen);
@@ -26,16 +33,57 @@ function App() {
       window.removeEventListener("resize", hideMenu);
     };
   });
+  let handleCallback = (data) => {
+    setImage1(data);
+  };
+  let handleCallback2 = (data) => {
+    setImage2(data);
+  };
+  let handleCallback3 = (data) => {
+    setImage3(data);
+  };
   return (
-    <div className="bg-black">
-      <Navbar toggle={toggle} />
-      <Dropdown isOpen={isOpen} toggle={toggle} />
-      <Hero />
-      <Gallery />
-      <Footer />
-    </div>
+    <>
+      <div className="w-full relative">
+        {image1 ? (
+          <img
+            className="absolute -z-1 w-full transform transition-all -translate-y-4 ease-out duration-500"
+            src={Pic1}
+            alt=""
+          />
+        ) : image2 ? (
+          <img
+            className="absolute -z-1 w-full transform transition-all -translate-y-4 ease-out duration-500"
+            src={Pic2}
+            alt=""
+          />
+        ) : image3 ? (
+          <img
+            className="absolute -z-1 w-full transform transition-all -translate-y-4 ease-out duration-500"
+            src={Pic3}
+            alt=""
+          />
+        ) : (
+          <img className="absolute -z-1 w-full" src={Bgatas1} alt="" />
+        )}
+
+        <img className="absolute -z-1 w-full" src={Bgatas2} alt="" />
+        <Navbar toggle={toggle} />
+        <Dropdown isOpen={isOpen} toggle={toggle} />
+        <Hero
+          callImage1={handleCallback}
+          callImage2={handleCallback2}
+          callImage3={handleCallback3}
+        />
+
+        <div className="overflow-hidden 2xl:pt-px xl:pt-28 bg-gradient-to-b from-colorbg1 via-colorbg1 to-colorbg2">
+          <Timeslide />
+          <Gallery />
+          <Footer />
+        </div>
+      </div>
+    </>
   );
- 
-}
+};
 
 export default App;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,28 +1,103 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import star1 from "../assets/image/bintang1.svg";
+import star2 from "../assets/image/bintang2.svg";
 
-const Hero = () => {
+const Hero = (props) => {
+  const [image1, setImage] = useState(false);
+  const [image2, setImage2] = useState(false);
+  const [image3, setImage3] = useState(false);
+
+  let changeImage = (e) => {
+    e.preventDefault();
+    setImage(!image1);
+    setImage2(false);
+    setImage3(false);
+  };
+
+  let changeImage2 = (e) => {
+    e.preventDefault();
+    setImage2(!image2);
+    setImage(false);
+    setImage3(false);
+  };
+  let changeImage3 = (e) => {
+    e.preventDefault();
+    setImage3(!image3);
+    setImage(false);
+    setImage2(false);
+  };
+
+  useEffect(() => {
+    props.callImage1(image1);
+    props.callImage2(image2);
+    props.callImage3(image3);
+  });
   return (
     <>
-      <div className="h-screen flex flex-col text-white ">
-        <div
-          className="text-2xl text-center font-medium pt-28 
+      <div className="z-10 relative ">
+        <img
+          src={star1}
+          alt=""
+          className="absolute xl:right-40 xl:top-64 lg:right-40 lg:top-10 md:right-20 md:w-20"
+          onClick={changeImage}
+        />
+        <img
+          src={star2}
+          alt=""
+          className="absolute right-1/3 top-80 lg:top-32 md:w-20 md:top-28"
+          onClick={changeImage2}
+        />
+        <img
+          src={star1}
+          alt=""
+          className="absolute xl:right-1/3 xl:top-1/3 lg:top-72 lg:w-12 md:w-12 md:top-40 md:right-32"
+          onClick={changeImage3}
+        />
+        <div className="h-auto flex flex-col text-white ">
+          <div
+            className="text-2xl text-center font-medium pt-28 
         2xl:text-5xl 2xl:pt-96 2xl:pl-72 2xl:text-left 
         xl:text-5xl xl:pt-96 xl:pl-64 xl:text-left 
         lg:text-4xl lg:pt-72 lg:pl-28 lg:text-left 
         md:text-3xl md:pt-40 md:pl-20 md:text-left 
         sm:text-3xl sm:text-left"
-        >
-          Everything you can <br /> imagine is real
-        </div>
-        <div
-          className="text-sm text-center font-normal italic pt-10 hidden
-        2xl:text-xl  2xl:pl-72 2xl:text-left 
-        xl:text-xl  xl:pl-64 xl:text-left 
-        lg:text-lg  lg:pl-28 lg:text-left 
-        md:text-md md:pl-20 md:text-left 
+          >
+            Everything you can <br /> imagine is real
+          </div>
+          <div
+            className="text-sm text-center font-normal italic pt-10 mb-1 hidden
+        2xl:text-xl  2xl:pl-72 2xl:text-left 2xl:mb-96
+        xl:text-xl  xl:pl-64 xl:text-left xl:mb-96
+        lg:text-lg  lg:pl-28 lg:text-left lg:mb-96
+        md:text-md md:pl-20 md:text-left md:mb-80
         sm:text-left sm:block"
-        >
-          *Try to Press The Star
+          >
+            *Try to Press The Star
+          </div>
+          <div
+            className="text-2xl text-center font-normal pt-28 
+        2xl:text-5xl 2xl:pt-96 2xl:mt-72 2xl:pl-72 2xl:text-left 
+        xl:text-5xl xl:pt-80 xl:pl-64 xl:text-left 
+        lg:text-4xl lg:pt-18 lg:pl-28 lg:text-left 
+        md:text-3xl md:pt-1 md:pl-20 md:text-left 
+        sm:text-3xl sm:text-left"
+          >
+            Omorphia
+          </div>
+          <div
+            className="text-sm text-center font-normal pt-5 w-full px-10
+        2xl:text-xl  2xl:pl-72 2xl:text-left 2xl:w-2/5 
+        xl:text-sm  xl:pl-64 xl:text-left xl:w-3/6
+        lg:text-sm  lg:pl-28 lg:text-left lg:w-3/6
+        md:text-xs md:pl-20 md:text-left md:w-4/6
+        sm:text-left sm:block"
+          >
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. If you are going to use a passage of Lorem Ipsum, you
+            need to be sure there isn't anything embarrassing hidden in
+          </div>
         </div>
       </div>
     </>
