d0a8cdad0f65f6b5d3ac41c0fd3f3ad96321ef9e
GagaPoloJr <rahmadiihaikal@gmail.com>
2021-03-06 23:52:27 +0700

    perbaiki conflict

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ export const App = () => {
         <Route path="/about">
           <About />
         </Route>
-        <Route path="/services"></Route>
         <Route path="/works">
         <Works/>
         </Route>
