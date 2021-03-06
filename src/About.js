58bed1e4de504e48fcd0490f2f3b59f5c68b6901
rickyrivaldo98@gmail.com <rickyrivaldo98@gmail.com>
2021-03-06 17:03:44 +0700

    about

--- src/About.js
+++ src/About.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import Footer from "./layout/Footer";
+import Navbar from "./layout/Navbar";
+import LogoAbout from "./assets/image/logoabout.svg";
 
 const About = () => {
-    return ( 
-        <>
-        <div>Halo</div>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <Navbar />
+      <div className="mt-40 w-full justify-center items-center flex flex-col mb-96">
+        <img src={LogoAbout} alt="" className="mb-5" />
+        <div className="md:w-1/3 "></div>
+        <div className="text-white text-center md:w-1/3 font-bold md:text-2xl ">
+          We are digging tunnels in creativity and paving the way for
+          aesthetics. The Phantom rules. We follow him crafting brands,
+          packaging, books – illustrating all the way into this occult
+          pilgrimage. How we do it.
+        </div>
+        <div className="md:w-1/3"></div>
+      </div>
+
+      <Footer />
+    </>
+  );
+};
+
 export default About;
