c6093ec32bb93d51c74c56b9f2a521399afe0654
GagaPoloJr <rahmadiihaikal@gmail.com>
2021-03-26 01:36:23 +0700

    buat bergerak di landing apge

--- src/components/Image.js
+++ src/components/Image.js
@@ -10,138 +10,137 @@ import Image4 from "../assets/image/gallery/4.png";
 const Mycomponent = () => {
   return (
     <SRLWrapper>
-      <div
-        className="animate-translateX"
-        style={{ transform: "rotate(-2deg)" }}
-      >
-        <div className="popup-gallery pb-5 flex">
-          <div className="px-3 items-center">
-            <Link to={Image1}>
-              <div
-                className="gallery-container gallery "
-                style={{ backgroundImage: `url(${Image1})` }}
-              >
-                <img
+      <div style={{ transform: "rotate(-2deg)" }}>
+        <div className="animate-translateX">
+          <div className="popup-gallery pb-5 flex">
+            <div className="px-3 items-center">
+              <Link to={Image1}>
+                <div
                   className="gallery-container gallery "
-                  alt="bedroom"
-                  src={Image1}
-                />
-              </div>
-            </Link>
-          </div>
-          <div className=" px-3 items-center">
-            <Link to={Image2}>
-              <div
-                className="gallery-container2 gallery "
-                style={{ backgroundImage: `url(${Image2})` }}
-              >
-                <img
-                  className="gallery-container2 gallery"
-                  src={Image2}
-                  alt="classroom"
-                />
-              </div>
-            </Link>
-          </div>
-          <div className="px-3 items-center">
-            <Link to={Image1}>
-              <div
-                className="gallery-container gallery "
-                style={{ backgroundImage: `url(${Image1})` }}
-              >
-                <img
+                  style={{ backgroundImage: `url(${Image1})` }}
+                >
+                  <img
+                    className="gallery-container gallery "
+                    alt="bedroom"
+                    src={Image1}
+                  />
+                </div>
+              </Link>
+            </div>
+            <div className=" px-3 items-center">
+              <Link to={Image2}>
+                <div
+                  className="gallery-container2 gallery "
+                  style={{ backgroundImage: `url(${Image2})` }}
+                >
+                  <img
+                    className="gallery-container2 gallery"
+                    src={Image2}
+                    alt="classroom"
+                  />
+                </div>
+              </Link>
+            </div>
+            <div className="px-3 items-center">
+              <Link to={Image1}>
+                <div
                   className="gallery-container gallery "
-                  alt="bedroom"
-                  src={Image1}
-                />
-              </div>
-            </Link>
+                  style={{ backgroundImage: `url(${Image1})` }}
+                >
+                  <img
+                    className="gallery-container gallery "
+                    alt="bedroom"
+                    src={Image1}
+                  />
+                </div>
+              </Link>
+            </div>
+            <div className=" px-3 items-center">
+              <Link to={Image2}>
+                <div
+                  className="gallery-container2 gallery "
+                  style={{ backgroundImage: `url(${Image2})` }}
+                >
+                  <img
+                    className="gallery-container2 gallery"
+                    src={Image2}
+                    alt="classroom"
+                  />
+                </div>
+              </Link>
+            </div>
           </div>
-          <div className=" px-3 items-center">
-            <Link to={Image2}>
-              <div
-                className="gallery-container2 gallery "
-                style={{ backgroundImage: `url(${Image2})` }}
-              >
-                <img
-                  className="gallery-container2 gallery"
-                  src={Image2}
-                  alt="classroom"
-                />
-              </div>
-            </Link>
-          </div>
-        </div>
-        <div className="popup-gallery transform -translate-x-60 relative flex flex-nowrap">
-          <div className=" px-3 items-center">
-            <Link to={Image2}>
-              <div
-                className="gallery-container gallery "
-                style={{ backgroundImage: `url(${Image2})` }}
-              >
-                <img
-                  className="gallery-container gallery"
-                  alt="classroom"
-                  src={Image2}
-                />
-              </div>
-            </Link>
-          </div>
-          <div className=" px-3 items-center">
-            <Link to={Image3}>
-              <div
-                className="gallery-container2 gallery  "
-                style={{ backgroundImage: `url(${Image3})` }}
-              >
-                <img
+          <div className="popup-gallery transform -translate-x-60 relative flex flex-nowrap">
+            <div className=" px-3 items-center">
+              <Link to={Image2}>
+                <div
+                  className="gallery-container gallery "
+                  style={{ backgroundImage: `url(${Image2})` }}
+                >
+                  <img
+                    className="gallery-container gallery"
+                    alt="classroom"
+                    src={Image2}
+                  />
+                </div>
+              </Link>
+            </div>
+            <div className=" px-3 items-center">
+              <Link to={Image3}>
+                <div
                   className="gallery-container2 gallery  "
-                  alt="bar room"
-                  src={Image3}
-                />
-              </div>
-            </Link>
-          </div>
-          <div className=" px-3 items-center">
-            <Link to={Image4}>
-              <div
-                className="gallery-container gallery "
-                style={{ backgroundImage: `url(${Image4})` }}
-              >
-                <img
+                  style={{ backgroundImage: `url(${Image3})` }}
+                >
+                  <img
+                    className="gallery-container2 gallery  "
+                    alt="bar room"
+                    src={Image3}
+                  />
+                </div>
+              </Link>
+            </div>
+            <div className=" px-3 items-center">
+              <Link to={Image4}>
+                <div
+                  className="gallery-container gallery "
+                  style={{ backgroundImage: `url(${Image4})` }}
+                >
+                  <img
+                    className="gallery-container gallery  "
+                    alt="working space"
+                    src={Image4}
+                  />
+                </div>
+              </Link>
+            </div>
+            <div className=" px-3 items-center">
+              <Link to={Image3}>
+                <div
+                  className="gallery-container2 gallery "
+                  style={{ backgroundImage: `url(${Image3})` }}
+                >
+                  <img
+                    className="gallery-container2 gallery  "
+                    alt="bar room"
+                    src={Image3}
+                  />
+                </div>
+              </Link>
+            </div>
+            <div className=" px-3 items-center">
+              <Link to={Image4}>
+                <div
                   className="gallery-container gallery  "
-                  alt="working space"
-                  src={Image4}
-                />
-              </div>
-            </Link>
-          </div>
-          <div className=" px-3 items-center">
-            <Link to={Image3}>
-              <div
-                className="gallery-container2 gallery "
-                style={{ backgroundImage: `url(${Image3})` }}
-              >
-                <img
-                  className="gallery-container2 gallery  "
-                  alt="bar room"
-                  src={Image3}
-                />
-              </div>
-            </Link>
-          </div>
-          <div className=" px-3 items-center">
-            <Link to={Image4}>
-              <div
-                className="gallery-container gallery  "
-                style={{ backgroundImage: `url(${Image4})` }}
-              >
-                <img
-                  className="gallery-container gallery  "
-                  alt="working space"
-                  src={Image4}
-                />
-              </div>
-            </Link>
+                  style={{ backgroundImage: `url(${Image4})` }}
+                >
+                  <img
+                    className="gallery-container gallery  "
+                    alt="working space"
+                    src={Image4}
+                  />
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
