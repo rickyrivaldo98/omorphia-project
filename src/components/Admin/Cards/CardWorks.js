51f34291d801629d058ecf4fee39e5a7fe08f504
GagaPoloJr <rahmadiihaikal@gmail.com>
2021-03-26 13:56:51 +0700

    nambah form gallery

--- src/components/Admin/Cards/CardWorks.js
+++ src/components/Admin/Cards/CardWorks.js
@@ -36,6 +36,12 @@ export default function CardWorks({ color }) {
               >
                 Gallery Works
               </h3>
+              <button
+                className="bg-green-500 text-white  font-bold  text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                type="button"
+              >
+                Add Gallery
+              </button>
             </div>
           </div>
         </div>
