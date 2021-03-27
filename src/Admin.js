85471a7b49ac1869c9376e3316e31e5b58aa308b
darknowns <doublezero13@gmail.com>
2021-03-27 14:37:29 +0700

    Login done

--- src/Admin.js
+++ src/Admin.js
@@ -28,15 +28,25 @@ const StyledAdmin = styled.div`
 const Admin = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const alert = useAlert();
 
   let history = useHistory();
 
   useEffect(() => {
     // setLoading(true);
     axios
-      .get("https://api.sarafdesign.com/checkUser")
+      .get("https://api.sarafdesign.com/checkUser", {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+        },
+      })
       .then((res) => {
-        if (res.status === 403) {
+        // history.push("/admin");
+        setLoading(false);
+      })
+      .catch((err) => {
+        // console.log(err);
+        if (err.response.status === 401) {
           history.push("/login");
           // setTimeout(() => {
           //   history.push("/login");
@@ -44,24 +54,10 @@ const Admin = () => {
           alert.show("Anda belum login");
           // console.log(error);
         }
-        history.push("/admin");
-      })
-      .catch((err) => {
-        // if (err.response) {
-        //   history.push("/login");
-        //   // setTimeout(() => {
-        //   //   history.push("/login");
-        //   // }, 1000);
-        //   alert("Anda belum login");
-        //   // console.log(error);
-        // }
+        // alert.show("Anda belum login");
+        // console.log(err);
       });
-    setLoading(false);
   }, []);
-
-  if (loading) {
-    return <div className="App"></div>;
-  }
 
   return (
     <>
